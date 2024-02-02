47deba0a7b31b35f0c613f53fef08a7dacb207ce
Sensei Keqing <k.izuka@yandex.com>
2024-02-03 02:24:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -98,5 +98,9 @@ async def restart_cmd(_, m: Message):
   await m.reply_text("Restarting Bot")
   quit(1)
 
+try:
+  uvloop.install()
+except:
+  pass
 print("<-- Bot Started Working -->")
 app.run()
